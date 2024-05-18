afd4ec5ec97061de5a997afa16c279e51373494a
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-05-18 04:45:08 +0100

    adjust index.html styling

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
   //Variables and Constant Initialization
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
-  const sliderItems = document.querySelectorAll(".slider-item");
-  let currentIndex = 0;
-  let intervalId;
+  const wDSliderItems = document.querySelectorAll(".wd-slider-item");
+  const dFSliderItems = document.querySelectorAll(".df-slider-item");
+  let wDCurrentIndex = 0;
+  let dFCurrentIndex = 0;
+
+  let wDIntervalId;
+  let dFIntervalId;
 
   //Slider Functions
-  function showSlide(index) {
-    sliderItems.forEach((item, i) => {
+  function wDShowSlide(index) {
+    wDSliderItems.forEach((item, i) => {
       if (i === index) {
         item.style.display = "block";
       } else {
@@ -18,39 +22,75 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  function nextSlide() {
-    currentIndex++;
-    if (currentIndex >= sliderItems.length) {
-      currentIndex = 0;
-    }
-    showSlide(currentIndex);
+  function dFShowSlide(index) {
+    dFSliderItems.forEach((item, i) => {
+      if (i === index) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
   }
 
-  function prevSlide() {
-    currentIndex--;
-    if (currentIndex < 0) {
-      currentIndex = sliderItems.length - 1;
+  //Navigation Function for Next Slider
+  function wDNextSlide() {
+    wDCurrentIndex++;
+    if (wDCurrentIndex >= wDSliderItems.length) {
+      wDCurrentIndex = 0;
     }
-    showSlide(currentIndex);
+    wDShowSlide(wDCurrentIndex);
   }
+
+  function dFNextSlide() {
+    dFCurrentIndex++;
+    if (dFCurrentIndex >= dFSliderItems.length) {
+      dFCurrentIndex = 0;
+    }
+    dFShowSlide(dFCurrentIndex);
+  }
+
+  //Navigation Function for Previous Slider
+  function wDPrevSlide() {
+    wDCurrentIndex--;
+    if (wDCurrentIndex < 0) {
+      wDCurrentIndex = wDSliderItems.length - 1;
+    }
+    wDShowSlide(wDCurrentIndex);
+  }
+
+  function dFPrevSlide() {
+    dFCurrentIndex--;
+    if (dFCurrentIndex < 0) {
+      dFCurrentIndex = dFSliderItems.length - 1;
+    }
+    dFShowSlide(dFCurrentIndex);
+  }
+
   //Auto slide and Button Event Listener
   function startAutoSlide() {
-    intervalId = setInterval(nextSlide, 3000); // Adjust interval timing as needed
+    wDIntervalId = setInterval(wDNextSlide, 3000);
+    dFIntervalId = setInterval(dFNextSlide, 3000);
   }
 
   function stopAutoSlide() {
-    clearInterval(intervalId);
+    clearInterval(wDIntervalId);
+    clearInterval(dFIntervalId);
   }
 
   prevBtn.addEventListener("click", () => {
     stopAutoSlide();
-    prevSlide();
+    wDPrevSlide();
+    dFPrevSlide();
   });
 
   nextBtn.addEventListener("click", () => {
     stopAutoSlide();
-    nextSlide();
+    wDNextSlide();
+    dFNextSlide();
   });
 
+  // Initial setup
   startAutoSlide();
+  wDShowSlide(wDCurrentIndex);
+  dFShowSlide(dFCurrentIndex);
 });
